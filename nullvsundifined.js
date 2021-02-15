7b37c649b64b7b277e896805f1e64b0d55024c67
abidhossainrion <abidhossainrion@gmail.com>
2021-02-15 23:58:32 +0600

    null vs undefined cleared for interview

---
+++ nullvsundifined.js
@@ -0,0 +1,22 @@
+let pakhi;
+
+function add(num1,num2){
+    console.log(num1+num2);
+    return
+}
+
+
+function add(num1){
+    console.log(num1,num2);
+}
+
+
+const pemik = {name:"smart dude",phone:647647}
+console.log(pemik.gf);
+
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2,5,9];
+console.log(ages[11]);
